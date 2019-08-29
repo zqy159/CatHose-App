d4adf119a5080462c6f6af4b239be2dc9e5bbd59
zqy159 <13097073991@163.com>
2019-08-30 01:12:46 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="index">
-      <div id="captureId" class="capture"   v-show="firstFlag">
-        <ul>
-          <li>itemi</li>
-          <li>itemi</li>
-          <li>itemi</li>
-          <li>itemi</li>
-          <li>itemi</li>
-          <li>itemi</li>
-          <li>itemi</li>
-        </ul>
-        <h2>helloworld</h2>
-      </div>
-    <img :src="dataURL" alt="" v-show="!firstFlag" width="200px">
+    <div id="captureId" class="capture" v-show="firstFlag">
+      <!-- <ul>
+        <li>itemi</li>
+        <li>itemi</li>
+        <li>itemi</li>
+        <li>itemi</li>
+        <li>itemi</li>
+        <li>itemi</li>
+        <li>itemi</li>
+      </ul> -->
+      <div id="qrcode" class="qrcode"></div>
+      <h2>helloworld</h2>
+    </div>
+    <img :src="dataURL" alt="" v-show="!firstFlag" >
   </div>
 </template>
 <script>
 import { qrcanvas } from 'qrcanvas';
-    import html2canvas from 'html2canvas';
-    export default {
-        data () {
-            return {
-                dataURL:'',
-                firstFlag:true,
-            };
-        },
-      
-        mounted(){
-            html2canvas(document.querySelector('#captureId')).then(canvas => {
-                let imgUrl = canvas.toDataURL('image/png');
-                this.dataURL = imgUrl;
-                this.firstFlag = false;
-            })
-        },
-        methods: {
+import html2canvas from 'html2canvas';
+export default {
+  data() {
+    return {
+      dataURL: '',
+      firstFlag: true,
+    };
+  },
+  created() {
+    this.$nextTick(function () {
+      //生成二维码
+      var canvas1 = qrcanvas({
+        data: decodeURIComponent('1'),
+        size: 50
+      });
+      document.getElementById("qrcode").innerHTML = '';
+      document.getElementById('qrcode').appendChild(canvas1);
 
-        }
-    }
+    })
+  },
+  mounted() {
+    html2canvas(document.querySelector('#captureId')).then(canvas => {
+    //   let imgUrl = canvas.toDataURL('image/png');
+         var imgUrl = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // 获取生成的图片的url 
+         console.log(imgUrl)
+    //   this.dataURL = imgUrl;
+      this.firstFlag = false;
+    })
+  },
+  methods: {
+
+  }
+}
 </script>
 <style lang='less' scoped>
 </style>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,13 +2,14 @@
 <!-- 分享图片生成 -->
 <template>
   <div class="container">
-    <div id="canvs">
+    <div id="canvs" v-if='show'>
       <div id="qrcode" class="qrcode"></div>
       <div class="title">如梦令·昨夜雨疏风骤</div>
       <div>[宋] 李清照</div>
       <div>昨夜雨疏风骤，浓睡不消残酒，试问卷帘人，却道海棠依旧。</div>
       <div>知否，知否，应是绿肥红瘦。</div>
     </div>
+    <img :src="imgUrl" alt="" v-else width="500"> 
     <div style="width: 400px;margin:50px auto;">
       <button class="button" type="button" value="button" @click="downloadForJS()">测试</button>
     </div>
@@ -25,13 +26,14 @@ export default {
   data() {
     return {
       imgUrl: '',
-      Gou
+      Gou,
+      show:true,
     }
   },
   watch: {
     imgUrl(val, oldval) {
       //监听到imgUrl有变化以后 说明新图片已经生成 隐藏DOM
-      this.$refs.box.style.display = "none";
+      // this.$refs.box.style.display = "none";
     }
   },
   created() {
@@ -44,6 +46,7 @@ export default {
       });
       document.getElementById("qrcode").innerHTML = '';
       document.getElementById('qrcode').appendChild(canvas1);
+      this.downloadForJS()
     })
   },
 
@@ -51,12 +54,14 @@ export default {
     //base64转blob
     downloadForJS() {
       //使用html2canvas 转换html为canvas
-      html2canvas(document.getElementById('canvs')).then(function (canvas) {
-        var imgUri = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // 获取生成的图片的url 　
-        var saveLink = document.createElement('a');
-        saveLink.href = imgUri;
-        saveLink.download = 'downLoad.png';
-        saveLink.click();
+      html2canvas(document.getElementById('canvs')).then((canvas)=> {
+       this.imgUrl   = canvas.toDataURL("image/png"); // 获取生成的图片的url
+        // console.log(imgUri) 　
+        this.show=false
+        // var saveLink = document.createElement('a');
+        // saveLink.href = imgUri;
+        // saveLink.download = 'downLoad.png';
+        // saveLink.click();
       });
     },
     base64ToBlob(code) {
