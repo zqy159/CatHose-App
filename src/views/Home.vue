2dbb4b6aa176841d05419e3b92d4de355a59bd13
zqy159 <13097073991@163.com>
2019-08-30 00:52:49 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,42 @@
 <template>
-<div class="app-content">
-	<div class="home pages">
-	    <h1 ref='p'>This is an home page</h1>
-    </div>
-	<!--装载子页面-->
-   <router-view></router-view>
-</div>
+  <div class="index">
+      <div id="captureId" class="capture"   v-show="firstFlag">
+        <ul>
+          <li>itemi</li>
+          <li>itemi</li>
+          <li>itemi</li>
+          <li>itemi</li>
+          <li>itemi</li>
+          <li>itemi</li>
+          <li>itemi</li>
+        </ul>
+        <h2>helloworld</h2>
+      </div>
+    <img :src="dataURL" alt="" v-show="!firstFlag" width="200px">
+  </div>
 </template>
-
 <script>
-import Vuex from 'vuex';
-export default {
-    data(){
-        return {
+import { qrcanvas } from 'qrcanvas';
+    import html2canvas from 'html2canvas';
+    export default {
+        data () {
+            return {
+                dataURL:'',
+                firstFlag:true,
+            };
+        },
+      
+        mounted(){
+            html2canvas(document.querySelector('#captureId')).then(canvas => {
+                let imgUrl = canvas.toDataURL('image/png');
+                this.dataURL = imgUrl;
+                this.firstFlag = false;
+            })
+        },
+        methods: {
 
         }
     }
-}
 </script>
-
-<style scoped>
-
+<style lang='less' scoped>
 </style>
